979528e715caba6a8fe2111e72bc57c26b8f38dc
haythembensouissi <haythembensouissi2@gmail.com>
2022-08-02 11:47:10 +0100

    styling the suggesstion list

--- Mun-TN/App.js
+++ Mun-TN/App.js
@@ -60,7 +60,7 @@ export default function App() {
           headerShown: false
         }}> 
       { user ? (
-        <Stack.Screen name="Welcome">
+        <Stack.Screen name="test">
         {props => <Tabe {...props} extraData={user} />}
         </Stack.Screen>
         ) : (
--- Mun-TN/src/screens/SuggestionList/SuggestionList.js
+++ Mun-TN/src/screens/SuggestionList/SuggestionList.js
@@ -1,6 +1,9 @@
-import { StyleSheet, Text, View, FlatList } from 'react-native'
+import { StyleSheet, Text, View,Image, FlatList, ListItem } from 'react-native'
 import React, { useEffect, useState } from 'react'
-import { firebase } from '../../../firebase/config'
+import {firebase} from "../../../firebase/config"
+import styles from './styles'
+import { FontAwesome } from '@expo/vector-icons';
+
 const RenderSuggessions = () => {
     const[list,setList]=useState([])
     const ref=firebase.firestore().collection('suggestions')
@@ -16,22 +19,36 @@ const RenderSuggessions = () => {
                     municipalityname:doc.data().municipalityname,
                     description:doc.data().description
                 })
-            })
+            }) 
             setList(list)
         })
     },[])
-  return (
+  return ( 
     <View>
-    <FlatList data={list} renderItem={({item})=>(
-        <View>
-        <Text>
+    <FlatList data={list} style={styles.container} renderItem={({item})=>(
+
+        <View style={styles.card}>
+        <Text style={styles.userName}>
         {item.type}
         </Text>
-        <Text>
+        <Text> 
         {item.description}
         </Text>
+        <Text> 
+        {item.municipalityname}
+        </Text>
+        
+      
+        <Image source={{uri:item.image.uri} } style={styles.feedImage}  /> 
+        <FontAwesome name="thumbs-up" color="red" size={(20)}/>
+        <View style={{ flexDirection: 'row', marginLeft: 10 }}>
+        <FontAwesome name="thumbs-down" color="gray" size={(20)}/> 
+ <Text style={{marginLeft: 5, fontSize: 10}}>5</Text> 
+
         </View>
-    )} />
+
+        </View>
+    )} /> 
     </View>
   )
 }
