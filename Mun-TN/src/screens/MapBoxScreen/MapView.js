2e23705262a59e5dfc24f8dd101e6fd844fa2e48
Azyz hssin <hssine.medaizz1@gmail.com>
2022-08-05 15:38:46 +0100

    map feature

--- Mun-TN/src/screens/MapBoxScreen/MapView.js
+++ Mun-TN/src/screens/MapBoxScreen/MapView.js
@@ -1,24 +1,42 @@
 import * as React from 'react';
+import Geolocation from '@react-native-community/geolocation';
 import MapView from 'react-native-maps';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <MapView style={styles.map} />
+import { StyleSheet, Text, View, Dimensions ,Image} from 'react-native';
+const latitudeDelta=0.025
+const longitudeDelta=0.025
+export default class MapViewComponent extends React.Component {
+  state={
+    region:{
+      latitudeDelta,
+      longitudeDelta,
+      latitude:33.7444613,
+      longitude:-118.3870173
+    }
+  }
+  componentDidMount(){
+    this.handleUserLocation();
+  }
+  handleUserLocation=()=>{
+    Geolocation.getCurrentPosition(pos=>{
+      alert(JSON.stringify(pos))
+    })
+  }
+onChangeValue = region=>{
+  /* alert(JSON.stringify(region)) */
+  this.setState({region})
+}
+  render() {
+    return(
+    <View style={{flex:1}}>
+      <MapView 
+      style={{flex:1}}
+      initialRegion = {this.state.region}
+      onRegionChangeComplete={this.onChangeValue}
+      ref= {ref=>this.map=ref}
+      />
+     
     </View>
   );
 }
+}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
-  },
-});
