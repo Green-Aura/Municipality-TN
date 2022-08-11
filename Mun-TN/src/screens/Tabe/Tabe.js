3b6516941bdc3285f3bdf6f7de4c897cbbbfdfef
haythembensouissi <haythembensouissi2@gmail.com>
2022-08-11 17:11:12 +0100

    merging

--- Mun-TN/src/screens/Tabe/Tabe.js
+++ Mun-TN/src/screens/Tabe/Tabe.js
@@ -85,7 +85,7 @@ export default function Tabe({focused, icon}) {
           }
         })}></Tab.Screen>
 
-        <Tab.Screen name={"SuggestionList"} component={SuggestionList} options={{
+        <Tab.Screen name={"Map"} component={MapViewComponent} options={{
           tabBarIcon: ({ focused }) => (
             <View style={{
               // centring Tab Button...
