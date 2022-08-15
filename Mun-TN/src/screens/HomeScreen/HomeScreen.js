60faba01463cf63b9b50b42a71acedf53641048f
Oussama-Fajraoui <oussama.fajraoui1@gmail.com>
2022-08-15 08:53:33 +0100

    pull

--- Mun-TN/src/screens/HomeScreen/HomeScreen.js
+++ Mun-TN/src/screens/HomeScreen/HomeScreen.js
@@ -56,7 +56,7 @@ const HomeScreen = (props) => {
                 source={{ uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4' }}
                 rate={1.0}
                 volume={1.0}
-                isMuted={false}
+                isMuted={true}
                 resizeMode="cover"
                 shouldPlay
                 isLooping
