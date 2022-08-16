d9cc90b85a5c796a5a9631970cfda6db83aa1506
Fawez11 <fawezferjaoui11@gmail.com>
2022-08-16 19:00:24 +0200

    fixing

--- Mun-TN/src/screens/Tabe/Tabe2.js
+++ Mun-TN/src/screens/Tabe/Tabe2.js
@@ -12,8 +12,8 @@ import SuggestionList from '../SuggestionList/SuggestionList'
 import * as Animatable from 'react-native-animatable';
 
 const TabArr = [
-  { route: 'Home', label: 'Home', type: Icons.Feather, icon: 'home', component: HomeScreen },
-  { route: 'Event', label: 'Event', type: Icons.SimpleLineIcons, icon: 'event', component: SuggestionList },
+  { route: 'Accueil', label: 'Accueil', type: Icons.Feather, icon: 'home', component: HomeScreen },
+  { route: 'liste de suggestions', label: 'liste de suggestions', type: Icons.Octicons, icon: 'checklist', component: SuggestionList },
   { route: 'Map', label: 'Map', type: Icons.SimpleLineIcons, icon: 'location-pin', component: MapViewComponent },
   { route: 'Profile', label: 'Profile', type: Icons.FontAwesome, icon: 'user-circle-o', component: Profile },
 ];
